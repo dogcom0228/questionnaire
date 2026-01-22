818efa131a607cc6063ce625db4effa30364af91
LiangJinTan <dogcom0228@gmaail.com>
2026-01-22 23:38:48 +0800

    feat: redesign admin layout with persistent sidebar and improved snackbar

--- resources/js/questionnaire/Layouts/AdminLayout.vue
+++ resources/js/questionnaire/Layouts/AdminLayout.vue
@@ -1,88 +1,152 @@
 <template>
     <v-app>
+        <!-- App Bar -->
         <v-app-bar
-            color="primary"
-            density="compact"
+            color="surface"
+            elevation="1"
+            density="default"
         >
-            <v-app-bar-nav-icon @click="drawer = !drawer" />
-            <v-toolbar-title>Questionnaire Admin</v-toolbar-title>
-            <v-spacer />
-            <v-btn icon="mdi-account" />
+            <v-app-bar-nav-icon
+                variant="text"
+                @click.stop="drawer = !drawer"
+                v-if="!mdAndUp"
+            ></v-app-bar-nav-icon>
+            <v-toolbar-title class="text-h6 font-weight-bold">
+                Questionnaire Admin
+            </v-toolbar-title>
         </v-app-bar>
 
+        <!-- Navigation Drawer -->
         <v-navigation-drawer
             v-model="drawer"
-            temporary
+            :permanent="mdAndUp"
+            :temporary="!mdAndUp"
+            elevation="2"
         >
-            <v-list nav>
+            <v-sheet
+                color="grey-lighten-4"
+                class="pa-4 d-flex align-center justify-center"
+            >
+                <v-avatar
+                    color="primary"
+                    size="64"
+                    class="mb-2"
+                >
+                    <span class="text-h5">QA</span>
+                </v-avatar>
+                <!-- Add user name if available in props/page -->
+            </v-sheet>
+
+            <v-divider></v-divider>
+
+            <v-list
+                nav
+                density="compact"
+            >
                 <v-list-item
                     prepend-icon="mdi-view-dashboard"
                     title="Dashboard"
-                    href="#"
-                    @click.prevent="navigate('questionnaire.admin.index')"
-                />
+                    :active="isRouteActive('questionnaire.admin.index')"
+                    color="primary"
+                    @click="navigate('questionnaire.admin.index')"
+                ></v-list-item>
+
                 <v-list-item
-                    prepend-icon="mdi-plus-circle"
-                    title="Create Questionnaire"
-                    href="#"
-                    @click.prevent="navigate('questionnaire.admin.create')"
-                />
+                    prepend-icon="mdi-plus-box"
+                    title="Create New"
+                    :active="isRouteActive('questionnaire.admin.create')"
+                    color="primary"
+                    @click="navigate('questionnaire.admin.create')"
+                ></v-list-item>
             </v-list>
         </v-navigation-drawer>
 
-        <v-main>
-            <slot />
+        <!-- Main Content -->
+        <v-main class="bg-grey-lighten-5">
+            <v-container
+                fluid
+                class="pa-6 fill-height align-start"
+            >
+                <slot />
+            </v-container>
         </v-main>
 
+        <!-- Global Snackbar -->
         <v-snackbar
             v-model="snackbar.show"
             :color="snackbar.color"
-            :timeout="3000"
+            :timeout="4000"
+            location="top right"
+            variant="elevated"
         >
-            {{ snackbar.message }}
+            <div class="d-flex align-center">
+                <v-icon
+                    :icon="snackbar.icon"
+                    class="mr-2"
+                ></v-icon>
+                {{ snackbar.message }}
+            </div>
+
             <template #actions>
                 <v-btn
                     variant="text"
+                    icon="mdi-close"
                     @click="snackbar.show = false"
-                >
-                    Close
-                </v-btn>
+                ></v-btn>
             </template>
         </v-snackbar>
     </v-app>
 </template>
 
 <script setup>
+    /* global route */
     import { ref, watch } from 'vue'
+    import { router, usePage } from '@inertiajs/vue3'
+    import { useDisplay } from 'vuetify'
 
-    const props = defineProps(['flash'])
-    const emit = defineEmits(['navigate'])
+    const { mdAndUp } = useDisplay()
+    const page = usePage()
 
-    const drawer = ref(false)
+    const drawer = ref(mdAndUp.value)
     const snackbar = ref({
         show: false,
         message: '',
-        color: 'success',
+        color: 'info',
+        icon: 'mdi-information',
     })
 
-    watch(() => props.flash, (newFlash) => {
-        if (newFlash?.success) {
-            snackbar.value = {
-                show: true,
-                message: newFlash.success,
-                color: 'success',
+    // Watch for flash messages
+    watch(
+        () => page.props.flash,
+        (newFlash) => {
+            if (newFlash?.success) {
+                showSnackbar(newFlash.success, 'success', 'mdi-check-circle')
+            } else if (newFlash?.error) {
+                showSnackbar(newFlash.error, 'error', 'mdi-alert-circle')
+            } else if (newFlash?.warning) {
+                showSnackbar(newFlash.warning, 'warning', 'mdi-alert')
+            } else if (newFlash?.info) {
+                showSnackbar(newFlash.info, 'info', 'mdi-information')
             }
-        }
-        if (newFlash?.error) {
-            snackbar.value = {
-                show: true,
-                message: newFlash.error,
-                color: 'error',
-            }
-        }
-    }, { deep: true, immediate: true })
+        },
+        { deep: true, immediate: true }
+    )
 
+    const showSnackbar = (message, color, icon) => {
+        snackbar.value = {
+            show: true,
+            message,
+            color,
+            icon,
+        }
+    }
+
+    // Navigation helper
     const navigate = (name) => {
-        emit('navigate', name)
+        router.visit(route(name))
+    }
+
+    const isRouteActive = (name) => {
+        return route().current(name)
     }
 </script>
